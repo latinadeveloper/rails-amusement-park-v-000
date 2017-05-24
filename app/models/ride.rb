a658c4c8aba82895cdf7006f827f905de4c708b1
Isis Tejeda <isis@isistejeda.com>
2017-05-23 20:43:19 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,11 +7,11 @@ class Ride < ActiveRecord::Base
   def take_ride
     errors = []
     if user.tickets < attraction.tickets
-      errors << "You do not have enough tickets to ride the Roller Coaster."
+      errors << "You do not have enough tickets to ride the #{attraction.name}."
     end
 
     if user.height < attraction.min_height
-      errors <<  "You are not tall enough to ride the Roller Coaster."
+      errors <<  "You are not tall enough to ride the #{attraction.name}."
     end
 
     unless errors.empty?
@@ -22,6 +22,7 @@ class Ride < ActiveRecord::Base
     user.nausea += attraction.nausea_rating
     user.happiness += attraction.happiness_rating
     user.save
+    "Thanks for riding the #{attraction.name}!"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
 
 
+  resources :users
+
+  resources :attractions  do
+    member do
+      post 'ride'
+    end
+  end
+
+  root 'users#home'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
+
 end
